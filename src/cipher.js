42e83d34d2f0ab05b636af50a6d59e32b62633fa
elaineshimabukuro <laineyoshizato@gmail.com>
2019-08-27 14:58:31 -0300

    testes rodados e ok

--- src/cipher.js
+++ src/cipher.js
@@ -1,33 +1,34 @@
 window.cipher = {
-  cipherEncode :function (MessageC, offsetcd) {
-    let valcrypt = ""
+  encode: cipherEncode,
+  decode: cipherDecode,
+};
+function cipherEncode(offsetcd, MessageC) {
+  let valcrypt = "";
 
-    for(let i=0; i<MessageC.length; i++){
-      let iLetter = MessageC[i].charCodeAt();
-      let newILetter = (iLetter - 65 + offsetcd) % 26 + 65;
-      let crpty =  String.fromCharCode(newILetter);
+  for (let i=0; i<MessageC.length; i++) {
+    let iLetter = MessageC[i].charCodeAt();
+    let newILetter = (iLetter - 65 + offsetcd) % 26 + 65;
+    let crpty = String.fromCharCode(newILetter);
 
-/*concatenação das letras percorridas*/
-      valcrypt = valcrypt + crpty;
-
-    }
-
-    return valcrypt
-  },
-
-  cipherDecode:function (MessageD, offsetdc){
-    let valDcrypt = ""
-
-    for(let i=0; i<MessageD.length; i++){
-      let iLetter = MessageD[i].charCodeAt();
-      console.log(iLetter)
-      let newILetter = (iLetter - 90 - offsetdc) %26 + 90;
-      let decrpty =  String.fromCharCode(newILetter);
-
-/*concatenação das letras percorridas*/
-      valDcrypt = valDcrypt + decrpty;
+    /*concatenação das letras percorridas*/
+    valcrypt = valcrypt + crpty;
   }
 
-    return valDcrypt;
+  return valcrypt;
 }
+
+function cipherDecode(offsetdc, MessageD) {
+  let valDcrypt = "";
+
+  for (let i=0; i<MessageD.length; i++) {
+    let iLetter = MessageD[i].charCodeAt();
+    let newILetter = (iLetter - 90 - offsetdc) %26 + 90;
+    let decrpty = String.fromCharCode(newILetter);
+
+    /*concatenação das letras percorridas*/
+    valDcrypt = valDcrypt + decrpty;
+  }
+
+  return valDcrypt;
+
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 /*função do botão de Codificar*/
-function btEncode (){
-  let MessageCd = document.getElementById('MessageC').value.toUpperCase();
-  let OffsetCd = Number(document.getElementById('offsetcd').value);
-  let valcrypt = window.cipher.cipherEncode(MessageCd, OffsetCd)
+document.getElementById("buttonCD").addEventListener("click", btEncode());
+function btEncode() {
+  let MessageCd = document.getElementById("MessageC").value.toUpperCase();
+  let OffsetCd = Number(document.getElementById("offsetcd").value);
+  let valcrypt = window.cipher.cipherEncode(MessageCd, OffsetCd);
 
   document.getElementById("valcrypt").innerHTML = valcrypt;
 }
 /*função do botão de Decodificar*/
-function btDecode (){
-  let MessageDc = document.getElementById('MessageD').value.toUpperCase();
-  let OffsetDc = Number(document.getElementById('offsetdc').value);
-  let valDcrypt = window.cipher.cipherDecode(MessageDc, OffsetDc)
+document.getElementById("buttonDC").addEventListener("click", btDecode());
+function btDecode() {
+  let MessageDc = document.getElementById("MessageD").value.toUpperCase();
+  let OffsetDc = Number(document.getElementById("offsetdc").value);
+  let valDcrypt = window.cipher.cipherDecode(MessageDc, OffsetDc);
 
   document.getElementById("valDcrypt").innerHTML = valDcrypt;
-  }
+}
