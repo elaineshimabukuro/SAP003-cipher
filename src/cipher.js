dbfe3fae172d9f31bc684a9b5836091c8b7dd122
elaineshimabukuro <laineyoshizato@gmail.com>
2019-08-29 17:36:02 -0300

    hacking edition nas functions

--- src/cipher.js
+++ src/cipher.js
@@ -8,8 +8,42 @@ function cipherEncode(offsetcd, MessageC) {
 
   for (let i=0; i<MessageC.length; i++) {
     let newChar = "";
+    let iLetter = MessageC[i].charCodeAt();
 
     if (MessageC[i] === " ") {
+      newChar = MessageC[i];
+    }
+     else if (64>=iLetter>=33) {
+       let newILetter = (iLetter - 33 + offsetcd) % 32 + 33;
+       let crpty = String.fromCharCode(newILetter);
+     }
+      else if (90>=iLetter>=65) {
+         let newILetter = (iLetter - 65 + offsetcd) % 26 + 65;
+         let crpty = String.fromCharCode(newILetter);
+      }
+        else if (96>=iLetter>=91) {
+          let newILetter = (iLetter - 91 + offsetcd) % 6 + 91;
+          let crpty = String.fromCharCode(newILetter);
+        }
+          else if (122>=iLetter>=97) {
+            let newILetter = (iLetter - 97 + offsetcd) % 26 + 97;
+            let crpty = String.fromCharCode(newILetter);
+          }
+          else (126>=iLetter>=123); {
+            let newILetter = (iLetter - 123 + offsetcd) % 4 + 123;
+            let crpty = String.fromCharCode(newILetter);
+          }
+
+      newChar = crpty;
+    }
+    // concatenação das letras percorridas
+    valcrypt = valcrypt + newChar;
+  }
+  console.log(valcrypt);
+
+
+
+    /*if (MessageC[i] === " ") {
       newChar = MessageC[i];
     }
     else {
@@ -24,7 +58,7 @@ function cipherEncode(offsetcd, MessageC) {
   }
 
   return valcrypt;
-}
+}*/
 
 function cipherDecode(offsetdc, MessageD) {
   let valDcrypt = "";
